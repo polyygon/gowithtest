178dd4edbc721b355dee6a070a127e66f9037d6b
Corey Crosby <coreylcrosby@gmail.com>
2019-12-13 12:20:37 -0700

    updating progress

--- main.go
+++ main.go
@@ -2,15 +2,22 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
+
+	if language == spanish {
+		return spanishHelloPrefix + name
+	}
+
 	return englishHelloPrefix + name //prints hello, world
 }
 
 func main() {
-	fmt.Println(Hello("Corey"))
+	fmt.Println(Hello("", ""))
 }
